9d1aff88026a2e7f4aef2a4626a208c6f292f5e2
Kevin Wang <kwangsan@gmail.com>
2019-01-27 08:45:32 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="Kevin Wang";
+config.admin.email="";
+config.app.name="reactjs-express-generator";
+config.app.url="";
+
+module.exports=config
